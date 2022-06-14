2501006269051b1afaf7c5298def92521044d5e0
unknown <joekaranjasenior52@gmail.com>
2022-06-14 13:11:46 +0300

    message

--- web-tasker/src/app/app.module.ts
+++ web-tasker/src/app/app.module.ts
@@ -3,19 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { LoginComponent } from './login/login.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import { LayoutModule } from './layout/layout.module';
+import { HomeComponent } from './home/home.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent,
-    LoginComponent
+    HomeComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
+    LayoutModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatToolbarModule
