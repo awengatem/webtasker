f2689c177d484374de350ade6f190e2c5f5e6acb
joe052 <joekaranjasenior52@gmail.com>
2022-07-27 18:25:53 +0300

    message

--- web-tasker/src/app/layout/auth/login/login.component.ts
+++ web-tasker/src/app/layout/auth/login/login.component.ts
@@ -1,9 +1,34 @@
 import { HttpResponse } from '@angular/common/http';
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import {
+  Component,
+  Directive,
+  ElementRef,
+  HostListener,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 import { data } from 'jquery';
 import { AccountService } from 'src/app/services/account-service.service';
 import { AuthService } from 'src/app/services/auth.service';
 import { TokenService } from 'src/app/services/token.service';
+import Validation from './validation';
+
+// /**directive helps detect form touched */
+// @Directive({
+//   selector: '[focus-field]',
+// })
+// export class FocusDirective {
+//   @HostListener('focusin', ['$event']) onFocus() {
+//     console.log('a field was focused!');
+//   }
+// }
 
 @Component({
   selector: 'app-login',
@@ -11,21 +36,25 @@ import { TokenService } from 'src/app/services/token.service';
   styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
-  //local variables
+  /**local variables */
   status: boolean = true;
+  submitted: boolean = false;
 
+  /**form used in login part */
   form: any = {
-    firstname: null,
-    lastname: null,
-    email: null,
     username: null,
     password: null,
   };
 
-  form2: any = {
-    username: null,
-    password: null,
-  };
+  /**form used in signup part */
+  fSignup: FormGroup = new FormGroup({
+    firstname: new FormControl(''),
+    lastname: new FormControl(''),
+    email: new FormControl(''),
+    username: new FormControl(''),
+    password: new FormControl(''),
+    confirmPassword: new FormControl(''),
+  });
 
   isLoggedIn = false;
   isLoginFailed = false;
@@ -38,15 +67,49 @@ export class LoginComponent implements OnInit {
   constructor(
     private authService: AuthService,
     private accountService: AccountService,
-    private tokenService: TokenService
+    private tokenService: TokenService,
+    private formBuilder: FormBuilder
   ) {}
 
   ngOnInit(): void {
+    /**helps during login */
     if (this.accountService.isLoggedIn()) {
       this.isLoggedIn = true;
       //this.roles = this.accountService.getUser().roles;
       this.user = this.accountService.getUserAccount();
     }
+
+    /**building form */
+    this.fSignup = this.formBuilder.group(
+      {
+        firstname: ['', [Validators.required, Validators.minLength(3)]],
+        lastname: ['', [Validators.required, Validators.minLength(3)]],
+        username: [
+          '',
+          [
+            Validators.required,
+            Validators.minLength(3),
+            Validators.maxLength(20),
+          ],
+        ],
+        email: ['', [Validators.required, Validators.email]],
+        password: [
+          '',
+          [
+            Validators.required,
+            Validators.minLength(6),
+            Validators.maxLength(20),
+          ],
+        ],
+        confirmPassword: ['', Validators.required],
+      },
+      { validators: [Validation.match('password', 'confirmPassword')] }
+    );
+
+    /**helps detect form touched */
+    this.fSignup.valueChanges.subscribe((res) => {
+      console.log('touching');
+    });
   }
 
   /*property to control styling of login and signup span elements*/
@@ -75,6 +138,45 @@ export class LoginComponent implements OnInit {
     console.log('loggged in');
   }
 
+  /**functions used by reactive form fSignup */
+
+  /**getter to access form.controls */
+  get f(): { [key: string]: AbstractControl } {
+    return this.fSignup.controls;
+  }
+
+  get firstname() {
+    return this.fSignup.get('firstname');
+  }
+
+  get lastname() {
+    return this.fSignup.get('lastname');
+  }
+
+  get username() {
+    return this.fSignup.get('username');
+  }
+
+  get email() {
+    return this.fSignup.get('email');
+  }
+
+  get password() {
+    return this.fSignup.get('password');
+  }
+
+  get confirmPassword() {
+    return this.fSignup.get('confirmPassword');
+  }
+
+  checkTouch() {
+    if (this.fSignup.touched) {
+      //this.submitted = false;
+    }
+
+    console.log('toooouch');
+  }
+
   /*login methods*/
   onLoginButtonClicked() {
     const { username, password } = this.form;
@@ -86,7 +188,7 @@ export class LoginComponent implements OnInit {
   }
 
   onSignin() {
-    const { username, password } = this.form;
+    const { username, password } = this.form; //data comes from template not here
     this.authService.login(username, password).subscribe({
       next: (data) => {
         console.log(data);
@@ -117,8 +219,9 @@ export class LoginComponent implements OnInit {
     });
   }
 
-  onSignup(){
-    console.log("signup submit button works!");
+  onSignup() {
+    this.submitted = true;
+    console.log('signup submit button works!');
   }
 
   reloadPage() {
--- web-tasker/src/app/layout/layout.module.ts
+++ web-tasker/src/app/layout/layout.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
@@ -42,6 +42,7 @@ import { RegisterComponent } from './auth/register/register.component';
     RouterModule,
     LayoutRoutingModule,
     FormsModule,
+    ReactiveFormsModule,
 
     //material imports
     AngularMaterialModule
