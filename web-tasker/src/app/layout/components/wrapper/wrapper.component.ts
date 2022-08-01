edc490d1b28a12c36f55090ff262d67aa6d0e2c0
joe052 <joekaranjasenior52@gmail.com>
2022-08-01 16:19:31 +0300

    message

--- web-tasker/src/app/layout/components/wrapper/wrapper.component.ts
+++ web-tasker/src/app/layout/components/wrapper/wrapper.component.ts
@@ -6,11 +6,6 @@ import {
   sideNavContainerAnimation,
 } from './wrapper.animations';
 
-interface sideNavToggle {
-  screenWidth: number;
-  isExpanded: boolean;
-}
-
 @Component({
   selector: 'app-wrapper',
   templateUrl: './wrapper.component.html',
@@ -18,10 +13,8 @@ interface sideNavToggle {
   animations: [sideNavAnimation, sideNavContainerAnimation],
 })
 export class WrapperComponent implements OnInit {
-  @Output() onToggleSideNav: EventEmitter<sideNavToggle> = new EventEmitter();
+  @Output() onToggleSideNav: EventEmitter<any> = new EventEmitter();
 
-  //variables
-  screenWidth = 0;
   isExpanded: boolean = true;
   isOpen = true;
 
@@ -30,21 +23,7 @@ export class WrapperComponent implements OnInit {
     private accountService: AccountService
   ) {}
 
-  ngOnInit(): void {
-    this.screenWidth = window.innerWidth;
-  }
-
-  toggleCollapse(): void {
-    if (this.isExpanded === true) {
-      this.isExpanded = false;
-    } else {
-      this.isExpanded = true;
-    }
-    this.onToggleSideNav.emit({
-      isExpanded: this.isExpanded,
-      screenWidth: this.screenWidth,
-    });
-  }
+  ngOnInit(): void {}
 
   toggle() {
     this.isOpen = !this.isOpen;
@@ -53,14 +32,6 @@ export class WrapperComponent implements OnInit {
     } else {
       this.isExpanded = true;
     }
-  }
-
-  closeSidenav(): void {
-    this.isExpanded = false;
-    this.onToggleSideNav.emit({
-      isExpanded: this.isExpanded,
-      screenWidth: this.screenWidth,
-    });
   }
 
   logout1() {
