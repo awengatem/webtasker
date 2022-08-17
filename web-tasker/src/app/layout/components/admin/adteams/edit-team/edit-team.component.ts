800e95cc728ed1a9035730a325d6a4a62e950dbc
joe052 <joekaranjasenior52@gmail.com>
2022-08-17 18:34:14 +0300

    message

--- web-tasker/src/app/layout/components/admin/adteams/edit-team/edit-team.component.ts
+++ web-tasker/src/app/layout/components/admin/adteams/edit-team/edit-team.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { TeamService } from 'src/app/services/team.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-edit-team',
@@ -6,10 +9,53 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-team.component.scss']
 })
 export class EditTeamComponent implements OnInit {
+  form: any = {
+    teamName: null,
+  };
+  submitted: boolean = false;
+  teamId!: string;
 
-  constructor() { }
+  constructor(
+    private teamService: TeamService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
+
+  //getting capturedTeam
+  capturedTeam!: string;
 
   ngOnInit(): void {
+    //subscribe to the route params
+    this.route.params.subscribe((params: Params) => {
+      this.teamId = params['teamId'];
+      console.log(this.teamId);
+    });
+
+    //get the selected team
+    this.capturedTeam = this.teamService.getCapturedTeam();
+    this.form.teamName = this.capturedTeam;
   }
 
+  /**edit method */
+  editTeam() {
+    const { teamName } = this.form; //data comes from template not here
+    this.teamService.editTeam(this.teamId, teamName).subscribe({
+      next: (response: any) => {
+        console.log(response);
+        //setting status to true to help in scrolldown method
+        this.teamService.setAddStatus(true);
+        //console.log(this.teamService.getAddStatus());
+        this.router.navigate(['/ad_teams']);
+        Swal.fire('Success!', `team "${teamName}" updated successfully`, 'success');        
+      },
+      error: (err) => {
+        console.log(err);
+        Swal.fire('Oops! Something went wrong',err.error, 'error');        
+      },
+    });
+  }
+
+  submit() {
+    this.submitted = true;
+  }
 }
--- web-tasker/src/app/layout/components/admin/adteams/new-team/new-team.component.ts
+++ web-tasker/src/app/layout/components/admin/adteams/new-team/new-team.component.ts
@@ -1,15 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { TeamService } from 'src/app/services/team.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-new-team',
   templateUrl: './new-team.component.html',
   styleUrls: ['./new-team.component.scss']
 })
-export class NewTeamComponent implements OnInit {
+export class NewTeamComponent implements OnInit {  
+  form: any = {
+    teamName: null,    
+  };
+  submitted: boolean = false;
 
-  constructor() { }
+  constructor(private teamService: TeamService, private router: Router) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  createTeam() {
+    const { teamName } = this.form; //data comes from template not here
+    this.teamService.createTeam(teamName).subscribe({
+      next: (response: any) => {
+        console.log(response);
+        //setting status to true to help in scrolldown method
+        this.teamService.setAddStatus(true);
+        //console.log(this.teamService.getAddStatus());
+        this.router.navigate(['/ad_teams']);       
+        Swal.fire('Success!', `team "${teamName}" created successfully`, 'success');
+      },
+      error: (err) => {
+        console.log(err);
+        Swal.fire('Oops! Something went wrong',err.error, 'error');        
+      },
+    });
   }
 
+  submit(){
+    this.submitted = true;
+  }
 }
