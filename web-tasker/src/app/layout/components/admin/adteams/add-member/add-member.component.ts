87a642fb30ed2bb37b91c2e1d3d0e5c5347740b2
joe052 <joekaranjasenior52@gmail.com>
2022-08-29 10:47:33 +0300

    message

--- web-tasker/src/app/layout/components/admin/adteams/add-member/add-member.component.ts
+++ web-tasker/src/app/layout/components/admin/adteams/add-member/add-member.component.ts
@@ -71,10 +71,10 @@ export class AddMemberComponent implements OnInit {
   //populating the dropdown list
   getUsers() {
     let tmp: any = [];
-    let filteredTmp: any = [];
+    let memberUsernames: any = [];
     let tmp2: any = [];
     let userArr: any = [];
-    let test: any = [];
+    let usernameArr: any = [];
 
     //get members already in team first
     this.teamService.getTeamMembers(this.teamId).subscribe({
@@ -84,41 +84,33 @@ export class AddMemberComponent implements OnInit {
         // });
         for (let i = 0; i < members.length; i++) {
           tmp2.push(members[i]);
+          //get only the usernames of members
+          memberUsernames.push(members[i].username);
         }
         this.members = tmp2;
-        console.log(this.members);
+        //console.log(this.members);
 
         //get all users in db
         this.teamService.getUsers().subscribe({
           next: (users: any) => {
             //push usernames and users to respective array
             for (let i = 0; i < users.length; i++) {
-              tmp.push({ item_id: i, item_text: users[i].username });
-              test.push(users[i].username);
+              usernameArr.push(users[i].username);
               userArr.push(users[i]);
             }
 
-            //filter out existing team members to avoid duplicates
-            for (let j = 0; j < this.members.length; j++) {
-              for (let i = 0; i < tmp.length; i++) {
-                //pick out duplicates
-                if (tmp[i].item_text === this.members[j].username) {
-                  filteredTmp.push(this.members[j].username);
-                }
-              }
+            //filter out members already in team
+            let filtered = usernameArr.filter(
+              (item: any) => !memberUsernames.includes(item)
+            );
+            //console.log(filtered);
+
+            //create dropdownlist array using filtered usernames
+            for (let i = 0; i < filtered.length; i++) {
+              tmp.push({ item_id: i, item_text: filtered[i] });
             }
-
-            //filter members
-            const filtered = users.forEach((user: any) => {
-              const filtered = test.filter((test: any) => {
-                test !== user.username;
-              });
-            });            
-
-            console.log(filteredTmp);
             //add usernames to dropdown list and users to array
             this.dropdownList = tmp;
-            //this.dropdownList = filteredTmp;
             this.userArr = userArr;
           },
           error: (err) => {
@@ -133,23 +125,7 @@ export class AddMemberComponent implements OnInit {
       },
     });
   }
-
-  /* //getting members already in team
-  getTeamMembers(teamId: string) {
-    let tmp: any = [];
-    this.teamService.getTeamMembers(teamId).subscribe((members: any) => {
-      // members.forEach((member: any) => {
-      //   console.log(member.username);
-      // });
-      for (let i = 0; i < members.length; i++) {
-        tmp.push(members[i]);
-      }
-      this.members = tmp;
-      console.log(this.members);
-      //return this.members;
-    });
-  }*/
-
+  
   addMember() {
     let teamMembers: any = [];
     //ensure members are selected
