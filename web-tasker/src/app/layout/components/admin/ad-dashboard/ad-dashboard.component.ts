a351f7a251819dbcad04c02c800e3fb5a47a8129
joe052 <joekaranjasenior52@gmail.com>
2022-09-15 13:12:27 +0300

    message

--- web-tasker/src/app/layout/components/admin/ad-dashboard/ad-dashboard.component.ts
+++ web-tasker/src/app/layout/components/admin/ad-dashboard/ad-dashboard.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { SocketIoService } from 'src/app/services/socket.io.service';
 
 @Component({
   selector: 'app-ad-dashboard',
@@ -7,46 +6,7 @@ import { SocketIoService } from 'src/app/services/socket.io.service';
   styleUrls: ['./ad-dashboard.component.scss'],
 })
 export class AdDashboardComponent implements OnInit {
-  title = 'Websocket Angular client ';
-  userName!: string;
-  message!: string;
-  output: any[] = [];
-  feedback!: string;
+  constructor() {}
 
-  constructor(private webSocketService: SocketIoService) {}
-  ngOnInit(): void {
-    this.webSocketService.listen('typing').subscribe((data) => {
-      this.updateFeedback(data);
-    });
-    this.webSocketService.listen('chat').subscribe((data) => {
-      this.updateMessage(data);
-    });
-  }
-
-  updateMessage(data: any) {
-    this.feedback = '';
-    if (!!!data) return;
-    this.output.push(data);
-  }
-
-  updateFeedback(data: any) {
-    this.feedback = `${data} is typing a message;`;
-  }
-
-  messageTyping(): void {
-    console.log(`${this.userName} is typing`);
-    this.webSocketService.emit('typing', this.userName);
-  }
-
-  sendMessage(): void {
-    console.log({
-      message: this.message,
-      handle: this.userName,
-    });
-    this.webSocketService.emit('chat', {
-      message: this.message,
-      handle: this.userName,
-    });
-    this.message = '';
-  }
+  ngOnInit(): void {}
 }
--- web-tasker/src/app/layout/components/admin/socket-test/socket-test.component.ts
+++ web-tasker/src/app/layout/components/admin/socket-test/socket-test.component.ts
@@ -1,15 +1,52 @@
 import { Component, OnInit } from '@angular/core';
+import { SocketIoService } from 'src/app/services/socket.io.service';
 
 @Component({
   selector: 'app-socket-test',
   templateUrl: './socket-test.component.html',
-  styleUrls: ['./socket-test.component.scss']
+  styleUrls: ['./socket-test.component.scss'],
 })
 export class SocketTestComponent implements OnInit {
+  title = 'Websocket Angular client ';
+  userName!: string;
+  message!: string;
+  output: any[] = [];
+  feedback!: string;
 
-  constructor() { }
-
+  constructor(private webSocketService: SocketIoService) {}
   ngOnInit(): void {
+    this.webSocketService.listen('typing').subscribe((data) => {
+      this.updateFeedback(data);
+    });
+    this.webSocketService.listen('chat').subscribe((data) => {
+      this.updateMessage(data);
+    });
   }
 
+  updateMessage(data: any) {
+    this.feedback = '';
+    if (!!!data) return;
+    this.output.push(data);
+  }
+
+  updateFeedback(data: any) {
+    this.feedback = `${data} is typing a message;`;
+  }
+
+  messageTyping(): void {
+    console.log(`${this.userName} is typing`);
+    this.webSocketService.emit('typing', this.userName);
+  }
+
+  sendMessage(): void {
+    console.log({
+      message: this.message,
+      handle: this.userName,
+    });
+    this.webSocketService.emit('chat', {
+      message: this.message,
+      handle: this.userName,
+    });
+    this.message = '';
+  }
 }
