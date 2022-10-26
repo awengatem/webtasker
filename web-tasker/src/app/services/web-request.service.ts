68d1012fc0b0bf2b81e6dd49cd9e1f7d7ca87bfc
joe052 <joekaranjasenior52@gmail.com>
2022-10-26 09:20:29 +0300

    message

--- web-tasker/src/app/services/web-request.service.ts
+++ web-tasker/src/app/services/web-request.service.ts
@@ -20,7 +20,7 @@ export class WebRequestService {
     /**for production testing purpose
      * rejects cookies but will fix later
      */
-    //this.ROOT_URL = 'http://192.168.5.120:3000';
+    // this.ROOT_URL = 'http://192.168.5.120:3000';
   }
 
   get(uri: string): Observable<any> {
