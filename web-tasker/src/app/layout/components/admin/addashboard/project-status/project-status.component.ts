4dfa60ec2c9c5850390c3f401698a61cf1ed1f14
joe052 <joekaranjasenior52@gmail.com>
2022-11-01 16:00:45 +0300

    message

--- web-tasker/src/app/layout/components/admin/addashboard/project-status/project-status.component.ts
+++ web-tasker/src/app/layout/components/admin/addashboard/project-status/project-status.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-project-status',
@@ -6,9 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./project-status.component.scss'],
 })
 export class ProjectStatusComponent implements OnInit {
+  @ViewChild('fields') fields!: ElementRef;
+
+  /**variables used by search and filter inputs */
+  selectedValue = '';
   searchText = '';
-  data = [1,1,1,1,1,1,1,1,1,1,1,1,1,1];
+  placeholder = 'enter username to search ...';
+
+  data = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
   constructor() {}
 
   ngOnInit(): void {}
+
+  /**what to do after select is changed */
+  onSelected(): void {
+    this.selectedValue = this.fields.nativeElement.value;
+    console.log(this.selectedValue);
+    this.searchText = `enter ${this.selectedValue} to search ...`;
+  }
 }
