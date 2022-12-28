03d1a62e4b45ec9e058ed957844f6bcf767235a5
joe052 <joekaranjasenior52@gmail.com>
2022-12-28 18:49:03 +0300

    Template material Table in mng users

--- web-tasker/src/app/layout/components/admin/manage/mng-users/mng-users.component.ts
+++ web-tasker/src/app/layout/components/admin/manage/mng-users/mng-users.component.ts
@@ -3,13 +3,26 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-mng-users',
   templateUrl: './mng-users.component.html',
-  styleUrls: ['./mng-users.component.scss']
+  styleUrls: ['./mng-users.component.scss'],
 })
 export class MngUsersComponent implements OnInit {
+  displayedColumns: string[] = [
+    'select',
+    'FirstName',
+    'LastName',
+    'DateofBirth',
+    'EmailId',
+    'Gender',
+    'Country',
+    'State',
+    'City',
+    'Address',
+    'Pincode',
+    'Edit',
+    'Delete',
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
