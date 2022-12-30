9d497488f215f96cec0540b61fed737fcedcff7b
joe052 <joekaranjasenior52@gmail.com>
2022-12-30 18:48:45 +0300

    message

--- web-tasker/src/app/layout/components/admin/manage/mng-users/mng-users.component.ts
+++ web-tasker/src/app/layout/components/admin/manage/mng-users/mng-users.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserAccountService } from 'src/app/services/api/user-account.service';
 
 @Component({
   selector: 'app-mng-users',
@@ -22,7 +23,21 @@ export class MngUsersComponent implements OnInit {
     'Delete',
   ];
 
-  constructor() {}
+  constructor(private userAccountService: UserAccountService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.getUsers();
+  }
+
+  /**Get the users */
+  getUsers(): void {
+    this.userAccountService.getUsers().subscribe({
+      next: (users) => {
+        console.log(users);
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
+  }
 }
