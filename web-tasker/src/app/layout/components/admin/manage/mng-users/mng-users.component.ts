46abb2bc5a9dd34a2d741e699e0ecf8a3c7d62ec
joe052 <joekaranjasenior52@gmail.com>
2022-12-30 20:46:42 +0300

    added material table

--- web-tasker/src/app/layout/components/admin/manage/mng-users/mng-users.component.ts
+++ web-tasker/src/app/layout/components/admin/manage/mng-users/mng-users.component.ts
@@ -1,5 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import {
+  MatSnackBar,
+  MatSnackBarHorizontalPosition,
+  MatSnackBarVerticalPosition,
+} from '@angular/material/snack-bar';
+import { MatDialog } from '@angular/material/dialog';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
 import { UserAccountService } from 'src/app/services/api/user-account.service';
+import { SelectionModel } from '@angular/cdk/collections';
 
 @Component({
   selector: 'app-mng-users',
@@ -7,23 +17,25 @@ import { UserAccountService } from 'src/app/services/api/user-account.service';
   styleUrls: ['./mng-users.component.scss'],
 })
 export class MngUsersComponent implements OnInit {
-  displayedColumns: string[] = [
-    'select',
-    'FirstName',
-    'LastName',
-    'DateofBirth',
-    'EmailId',
-    'Gender',
-    'Country',
-    'State',
-    'City',
-    'Address',
-    'Pincode',
-    'Edit',
-    'Delete',
-  ];
+  employeeForm: any;
+  dataSource!: MatTableDataSource<any>;
+  selection = new SelectionModel<any>(true, []);
+  displayedColumns: string[] = ['select', 'FirstName'];
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
 
-  constructor(private userAccountService: UserAccountService) {}
+  constructor(private userAccountService: UserAccountService) {
+    this.userAccountService.getUsers().subscribe({
+      next: (users) => {
+        this.dataSource = new MatTableDataSource(users);
+        this.dataSource.paginator = this.paginator;
+        this.dataSource.sort = this.sort;
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
+  }
 
   ngOnInit(): void {
     this.getUsers();
@@ -39,5 +51,26 @@ export class MngUsersComponent implements OnInit {
         console.log(err);
       },
     });
+  }
+
+  isAllSelected() {
+    const numSelected = this.selection.selected.length;
+    const numRows = !!this.dataSource && this.dataSource.data.length;
+    return numSelected === numRows;
+  }
+  /** Selects all rows if they are not all selected; otherwise clear selection. */
+  masterToggle() {
+    this.isAllSelected()
+      ? this.selection.clear()
+      : this.dataSource.data.forEach((r) => this.selection.select(r));
+  }
+  /** The label for the checkbox on the passed row */
+  checkboxLabel(row: any): string {
+    if (!row) {
+      return `${this.isAllSelected() ? 'select' : 'deselect'} all`;
+    }
+    return `${this.selection.isSelected(row) ? 'deselect' : 'select'} row ${
+      row.EmpId + 1
+    }`;
   }
 }
