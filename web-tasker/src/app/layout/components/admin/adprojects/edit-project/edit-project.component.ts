d7375d99596a05638f2a720d7ec5ff1622979aee
joe052 <joekaranjasenior52@gmail.com>
2023-01-06 17:59:22 +0300

    tracking previous route

--- web-tasker/src/app/layout/components/admin/adprojects/edit-project/edit-project.component.ts
+++ web-tasker/src/app/layout/components/admin/adprojects/edit-project/edit-project.component.ts
@@ -33,7 +33,7 @@ export class EditProjectComponent implements OnInit {
     //get the selected project
     if (this.projectId) {
       this.loadFieldsToEdit(this.projectId);
-      document.getElementById("autofocus")!.focus();
+      document.getElementById('autofocus')!.focus();
     }
   }
 
@@ -47,8 +47,8 @@ export class EditProjectComponent implements OnInit {
         console.log(response);
         //setting status to true to help in scrolldown method
         this.projectService.setAddStatus(true);
-        //console.log(this.projectService.getAddStatus());
-        this.router.navigate(['/ad_projects']);
+        //console.log(this.projectService.getAddStatus());       
+        this.navigateBack();
         Swal.fire(
           'Success!',
           `Project "${newProject}" updated successfully`,
@@ -68,6 +68,20 @@ export class EditProjectComponent implements OnInit {
       console.log(project);
       this.form.projectName = project.projectName;
     });
+  }
+
+  /**Method to navigate to previous route */
+  navigateBack() {
+    //check if previous location is from manage component
+    let fromMngProj = window.sessionStorage.getItem('fromMngProj');
+    if (fromMngProj === 'true') {
+      //navigate to manager
+      this.router.navigate(['ad_manage/projects']);
+      //clear previous location
+      window.sessionStorage.removeItem('fromMngProj');
+    } else {
+      this.router.navigate(['/ad_projects']);
+    }
   }
 
   submit() {
