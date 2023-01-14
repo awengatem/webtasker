6b59e48fef7d7696c074f7d980db3bb360929342
joe052 <joekaranjasenior52@gmail.com>
2023-01-14 06:12:03 +0300

    updated sessions component

--- web-tasker/src/app/layout/components/user/user_sessions/sessions/sessions.component.ts
+++ web-tasker/src/app/layout/components/user/user_sessions/sessions/sessions.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-sessions',
   templateUrl: './sessions.component.html',
-  styleUrls: ['./sessions.component.scss']
+  styleUrls: ['./sessions.component.scss'],
 })
 export class SessionsComponent {
+  sessions!: any[];
+  sessionsLength = 0;
+  /**used by  the search bar */
+  searchText = '';
 
+  constructor() {}
+
+  ngOnInit(): void {}
 }
