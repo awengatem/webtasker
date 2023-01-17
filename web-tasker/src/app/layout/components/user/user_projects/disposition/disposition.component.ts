9b633b88efef6bc47e401ac8f5e8029454833264
joe052 <joekaranjasenior52@gmail.com>
2023-01-17 21:58:46 +0300

    added radio button list in disposition component

--- web-tasker/src/app/layout/components/user/user_projects/disposition/disposition.component.ts
+++ web-tasker/src/app/layout/components/user/user_projects/disposition/disposition.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Params, Router } from '@angular/router';
 import { DispositionService } from 'src/app/services/api/disposition.service';
 import { GeneralService } from 'src/app/services/general.service';
@@ -11,17 +12,20 @@ import Swal from 'sweetalert2';
   styleUrls: ['./disposition.component.scss'],
 })
 export class DispositionComponent implements OnInit {
+  form: FormGroup = new FormGroup({});
+  // form: any = {
+  //   other: null,
+  // };
   reason: string = 'Short break';
-  reasons: string[] = ['Tea break', 'Lunch break', 'Short break', 'Other'];
-  form: any = {
-    reason: null,
-  };
+  reasons: string[] = ['Tea break', 'Lunch break', 'Short break'];
   submitted: boolean = false;
+  isTextAreaOpen: boolean = false;
   projectId!: string;
 
   constructor(
     private dispositionService: DispositionService,
     private generalService: GeneralService,
+    private fb: FormBuilder,
     private webSocketService: SocketIoService,
     private router: Router,
     private route: ActivatedRoute
@@ -32,34 +36,46 @@ export class DispositionComponent implements OnInit {
       const projectId = params['projectId'];
       this.projectId = projectId;
     });
+
+    /**build form */
+    this.form = this.fb.group({
+      other: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(4),
+          Validators.maxLength(50),
+        ],
+      ],
+    });
   }
 
   /**method to post reasom for pause to db */
   createDisposition() {
-    const { reason } = this.form; //data from template not here
-    //remove unneccessary whitespace
-    const cleanReason = this.generalService.clean(reason);
-    this.dispositionService.createDisposition(cleanReason).subscribe({
-      next: (response: any) => {
-        console.log(response);
-        this.pauseTimer()
-          .then(() => {
-            Swal.fire(
-              'Success!',
-              `Your reason has been submitted and session paused successfully.`,
-              'success'
-            );
-          })
-          .catch((err) => {
-            console.log(err);
-          });
-        this.router.navigate([`/projects/${this.projectId}/action`]);
-      },
-      error: (err) => {
-        console.log(err);
-        Swal.fire('Oops! Something went wrong', err.error.message, 'error');
-      },
-    });
+    // const { reason } = this.form; //data from template not here
+    // //remove unneccessary whitespace
+    // const cleanReason = this.generalService.clean(reason);
+    // this.dispositionService.createDisposition(cleanReason).subscribe({
+    //   next: (response: any) => {
+    //     console.log(response);
+    //     this.pauseTimer()
+    //       .then(() => {
+    //         Swal.fire(
+    //           'Success!',
+    //           `Your reason has been submitted and session paused successfully.`,
+    //           'success'
+    //         );
+    //       })
+    //       .catch((err) => {
+    //         console.log(err);
+    //       });
+    //     this.router.navigate([`/projects/${this.projectId}/action`]);
+    //   },
+    //   error: (err) => {
+    //     console.log(err);
+    //     Swal.fire('Oops! Something went wrong', err.error.message, 'error');
+    //   },
+    // });
   }
 
   /**method to pause timer */
@@ -70,7 +86,21 @@ export class DispositionComponent implements OnInit {
     });
   }
 
-  submit() {
+  /**method to show text area */
+  toggleTextArea(event: any, form: any) {
+    const { other } = form.value;
+    /**Show textarea if checked*/
+    if (event.value === other) {
+      console.log('other checked');
+      this.isTextAreaOpen = true;
+    } else {
+      this.isTextAreaOpen = false;
+    }
+  }
+
+  /**Method to submit the form */
+  submitForm(form: any) {
     this.submitted = true;
+    console.log(form.value);
   }
 }
