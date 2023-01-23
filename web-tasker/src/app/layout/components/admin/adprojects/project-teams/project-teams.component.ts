337ebce8f988879754cd6813e70b9ecbae54ee2f
joe052 <joekaranjasenior52@gmail.com>
2023-01-23 15:06:12 +0300

    progress on project teams

--- web-tasker/src/app/layout/components/admin/adprojects/project-teams/project-teams.component.ts
+++ web-tasker/src/app/layout/components/admin/adprojects/project-teams/project-teams.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import { ProjectService } from 'src/app/services/api/project.service';
 
 @Component({
   selector: 'app-project-teams',
@@ -8,10 +9,16 @@ import { ActivatedRoute, Params } from '@angular/router';
 })
 export class ProjectTeamsComponent implements OnInit {
   projectId!: string;
+  projectName: any;
+  teamCount: any;
+  teams!: any[];
   /**used by search bar */
   searchText = '';
 
-  constructor(private route: ActivatedRoute) {}
+  constructor(
+    private route: ActivatedRoute,
+    private projectService: ProjectService
+  ) {}
 
   ngOnInit(): void {
     //subscribe to the route params
@@ -19,11 +26,45 @@ export class ProjectTeamsComponent implements OnInit {
       console.log(params);
       const projectId = params['projectId'];
       this.projectId = projectId;
+      if (projectId) {
+        this.getProject(projectId);
+        this.getProjectTeams(projectId);
+      }
+    });
+  }
+
+  /**Get project name */
+  getProject(projectId: string) {
+    this.projectService.getSpecificProject(projectId).subscribe({
+      next: (project) => {
+        if (project) {
+          this.projectName = project.projectName
+            ? project.projectName
+            : 'Project name';
+          this.teamCount = project.teams.length ? project.teams.length : 0;
+        } else {
+          this.projectName = 'Project name';
+          this.teamCount = 0;
+        }
+      },
+      error: (err) => {
+        console.log(err);
+      },
     });
   }
 
   /**get teams */
-  getProjectTeams(projectId: string){
-
+  getProjectTeams(projectId: string) {
+    this.projectService.getProjectTeams(projectId).subscribe({
+      next: (teams) => {
+        console.log(teams);
+        if (teams) {
+          this.teams = teams;
+        }
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
   }
 }
