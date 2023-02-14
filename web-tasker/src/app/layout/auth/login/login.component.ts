cc66bb1412d7ae211bc13a1ab1de93ba8d7c960e
joe052 <joekaranjasenior52@gmail.com>
2023-02-14 17:56:32 +0300

    message

--- web-tasker/src/app/layout/auth/login/login.component.ts
+++ web-tasker/src/app/layout/auth/login/login.component.ts
@@ -66,6 +66,22 @@ export class LoginComponent implements OnInit {
     this.currentPage--;
   }
 
+  config = {
+    displayFn: (item: any) => {
+      return item.hello.world;
+    }, //a replacement ofr displayKey to support flexible text displaying for each item
+    displayKey: 'description', //if objects array passed which key to be displayed defaults to description
+    search: true, //true/false for the search functionlity defaults to false,
+    height: 'auto', //height of the list so that if there are more no of items it can show a scroll defaults to auto. With auto height scroll will never appear
+    placeholder: 'Select', // text to be displayed when no item is selected defaults to Select,
+    customComparator: () => {}, // a custom function using which user wants to sort the items. default is undefined and Array.sort() will be used in that case,
+    limitTo: 0, // number thats limits the no of options displayed in the UI (if zero, options will not be limited)
+    moreText: 'more', // text to be displayed whenmore than one items are selected like Option 1 + 5 more
+    noResultsFound: 'No results found!', // text to be displayed when no items are found while searching
+    searchPlaceholder: 'Search', // label thats displayed in search input,
+    searchOnKey: 'name', // key on which search should be performed this will be selective search. if undefined this will be extensive search on all keys
+  };
+
   /**form used in signup part */
   fSignup1: FormGroup = new FormGroup({
     firstname: new FormControl(''),
