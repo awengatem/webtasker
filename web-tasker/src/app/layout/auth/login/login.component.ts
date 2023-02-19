06ccef95a36c09a90f07226a52bee1bcf2f9ae30
joe052 <joekaranjasenior52@gmail.com>
2023-02-20 00:11:35 +0300

    message

--- web-tasker/src/app/layout/auth/login/login.component.ts
+++ web-tasker/src/app/layout/auth/login/login.component.ts
@@ -51,8 +51,6 @@ export class LoginComponent implements OnInit {
     password: null,
   };
 
-  dataModel: any = [];
-
   //test
   currentPage = 3;
   name = '';
@@ -99,22 +97,6 @@ export class LoginComponent implements OnInit {
   date: any;
   genders: any = ['male', 'female', 'other'];
   options = ['broo1', 'broo2', 'broo3', 'broo4', 'broo5', 'broo6'];
-  config = {
-    displayKey: 'description', //if objects array passed which key to be displayed defaults to description
-    search: true, //true/false for the search functionlity defaults to false,
-    height: '600%', //height of the list so that if there are more no of items it can show a scroll defaults to auto. With auto height scroll will never appear
-    placeholder: 'Select', // text to be displayed when no item is selected defaults to Select,
-    customComparator: () => {
-      return 0;
-    }, // a custom function using which user wants to sort the items. default is undefined and Array.sort() will be used in that case,
-    limitTo: this.options.length, // a number thats limits the no of options displayed in the UI similar to angular's limitTo pipe
-    moreText: 'more', // text to be displayed whenmore than one items are selected like Option 1 + 5 more
-    noResultsFound: 'No results found!', // text to be displayed when no items are found while searching
-    searchPlaceholder: 'Search', // label thats displayed in search input,
-    searchOnKey: 'name', // key on which search should be performed this will be selective search. if undefined this will be extensive search on all keys
-    clearOnSelection: false, // clears search criteria when an option is selected if set to true, default is false
-    inputDirection: 'ltr', // the direction of the search input can be rtl or ltr(default)
-  };
 
   ngOnInit(): void {
     /**helps during login */
@@ -227,14 +209,11 @@ export class LoginComponent implements OnInit {
     });
   }
 
-  /**used by dropdown */
-  selectionChanged(e: any) {
-    this.dataModel = e.value;
-    console.log(this.dataModel);
-  }
-
-  myFunction() {
-    document.getElementById('myDropdown')!.classList.toggle('show');
+  /**Method to detect selection of county */
+  changeCounty(e: any) {
+    this.county?.setValue(e.target.value, {
+      onlySelf: true,
+    });
   }
 
   /*methods used by header buttons*/
@@ -410,7 +389,6 @@ export class LoginComponent implements OnInit {
 
   subTest() {
     console.log('test accomplished!!');
-    console.log(this.dataModel);
   }
 
   validateLogin() {
