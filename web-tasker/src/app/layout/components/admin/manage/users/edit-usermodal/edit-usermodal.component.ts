ccb4372a0e04c42f8fa945b39107b43202af5987
joe052 <joekaranjasenior52@gmail.com>
2023-02-24 19:28:28 +0300

    finished uo editting form

--- web-tasker/src/app/layout/components/admin/manage/users/edit-usermodal/edit-usermodal.component.ts
+++ web-tasker/src/app/layout/components/admin/manage/users/edit-usermodal/edit-usermodal.component.ts
@@ -204,7 +204,7 @@ export class EditUsermodalComponent implements OnInit {
       this.form.controls['email'].setValue(user.email);
       this.form.controls['firstName'].setValue(user.firstName);
       this.form.controls['lastName'].setValue(user.lastName);
-      this.form.controls['dob'].setValue(user.dob);
+      this.form.controls['dob'].setValue(new Date(user.dob));
       this.form.controls['idNo'].setValue(user.idNumber);
       this.form.controls['gender'].setValue(user.gender);
       this.form.controls['telNo'].setValue(user.telNumber);
