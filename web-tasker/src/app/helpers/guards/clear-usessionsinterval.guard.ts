e3ebecf5cfbe54eb731ed9c1605e8689494b075e
joe052 <joekaranjasenior52@gmail.com>
2023-05-04 22:26:59 +0300

    message

---
+++ web-tasker/src/app/helpers/guards/clear-usessionsinterval.guard.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { CanDeactivate } from '@angular/router';
+import { Observable } from 'rxjs';
+
+export interface CanComponentDeactivate {
+  canDeactivate: () => Observable<boolean> | Promise<boolean> | boolean;
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ClearUsessionsintervalGuard
+  implements CanDeactivate<CanComponentDeactivate>
+{
+  canDeactivate(
+    component: CanComponentDeactivate
+  ): Observable<boolean> | Promise<boolean> | boolean {
+    /**Perform the action here before the user leaves the route*/
+    //capture the dashboard interval to terminate
+    const sessionInterval = parseInt(
+      localStorage.getItem('user-sessions-interval')!
+    );
+    if (sessionInterval) {
+      window.clearInterval(sessionInterval);
+      console.log('Sessions refresh interval terminated');
+    }
+    localStorage.removeItem('user-sessions-interval');
+
+    return component.canDeactivate ? component.canDeactivate() : true;
+  }
+}
