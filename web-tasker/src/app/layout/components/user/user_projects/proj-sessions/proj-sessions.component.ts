94215e6a9a3dcc3889d3650f15b6ee40e55bc7b8
joe052 <joekaranjasenior52@gmail.com>
2023-05-19 01:06:58 +0300

    message

---
+++ web-tasker/src/app/layout/components/user/user_projects/proj-sessions/proj-sessions.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-proj-sessions',
+  templateUrl: './proj-sessions.component.html',
+  styleUrls: ['./proj-sessions.component.scss']
+})
+export class ProjSessionsComponent {
+
+}
