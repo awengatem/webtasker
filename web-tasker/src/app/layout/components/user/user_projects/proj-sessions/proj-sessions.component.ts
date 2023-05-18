d289dd25e81a3c6fcff12aa4f64636456e3802f2
joe052 <joekaranjasenior52@gmail.com>
2023-05-19 01:40:28 +0300

    message

--- web-tasker/src/app/layout/components/user/user_projects/proj-sessions/proj-sessions.component.ts
+++ web-tasker/src/app/layout/components/user/user_projects/proj-sessions/proj-sessions.component.ts
@@ -3,8 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-proj-sessions',
   templateUrl: './proj-sessions.component.html',
-  styleUrls: ['./proj-sessions.component.scss']
+  styleUrls: ['./proj-sessions.component.scss'],
 })
 export class ProjSessionsComponent {
-
+  constructor() {}
+  ngOnInit(): void {}
 }
