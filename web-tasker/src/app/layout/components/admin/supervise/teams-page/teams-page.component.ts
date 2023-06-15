1eb758777f642e079d63e4cc750791c30c2557b2
Immanuel Kimani <immanuel4082@gmail.com>
2023-06-15 06:59:22 +0300

    message

--- web-tasker/src/app/layout/components/admin/supervise/teams-page/teams-page.component.ts
+++ web-tasker/src/app/layout/components/admin/supervise/teams-page/teams-page.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-teams-page',
   templateUrl: './teams-page.component.html',
-  styleUrls: ['./teams-page.component.scss']
+  styleUrls: ['./teams-page.component.scss'],
 })
 export class TeamsPageComponent {
+  members: any = [];
+  projects: any = [];
+  constructor(private router: Router) {}
 
+  goBack() {
+    this.router.navigate(['/supervise_teams']);
+  }
+
+  ngOnInit(): void {
+    this.members = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
+    this.projects = [1, 1, 1, 1, 1, 1];
+  }
 }
