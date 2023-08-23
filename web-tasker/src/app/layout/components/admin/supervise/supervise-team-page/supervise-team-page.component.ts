a31a2096ad6e51bf81dbeb97ee398ef10041fe3f
joe052 <joekaranjasenior52@gmail.com>
2023-08-23 21:21:55 +0300

    message

--- web-tasker/src/app/layout/components/admin/supervise/supervise-team-page/supervise-team-page.component.ts
+++ web-tasker/src/app/layout/components/admin/supervise/supervise-team-page/supervise-team-page.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-supervise-team-page',
   templateUrl: './supervise-team-page.component.html',
-  styleUrls: ['./supervise-team-page.component.scss']
+  styleUrls: ['./supervise-team-page.component.scss'],
 })
-export class SuperviseTeamPageComponent {
+export class SuperviseTeamPageComponent implements OnInit {
+  constructor() {}
 
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
 }
