a64e64f72c16e6f2319155791c8800db49584811
joe052 <joekaranjasenior52@gmail.com>
2023-08-30 21:47:46 +0300

    message

--- web-tasker/src/app/helpers/guards/admin.guard.ts
+++ web-tasker/src/app/helpers/guards/admin.guard.ts
@@ -1,15 +1,40 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  Router,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
 import { Observable } from 'rxjs';
+import { AuthService } from 'src/app/services/auth.service';
+import Swal from 'sweetalert2';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AdminGuard implements CanActivate {
+  constructor(private authService: AuthService, private router: Router) {}
+
   canActivate(
     route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+    state: RouterStateSnapshot
+  ):
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
+    return new Promise((resolve, reject) => {
+      this.authService.verifyAdmin().then((result) => {
+        if (result === true) {
+          resolve(true);
+        } else {
+          /**notify user */
+          Swal.fire('Unauthorized!', `You are not an admin.`, 'warning');
+          this.router.navigate(['/home']);
+          resolve(false);
+        }
+      });
+    });
   }
-  
 }
