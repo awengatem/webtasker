522e1ea3e0f4172f1220024af7ae9cc374a2e984
joe052 <joekaranjasenior52@gmail.com>
2023-11-16 01:55:03 +0300

    message

---
+++ web-tasker/src/app/layout/components/admin/manager/adprojects/ad-project-info-modal/ad-project-info-modal.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ad-project-info-modal',
+  templateUrl: './ad-project-info-modal.component.html',
+  styleUrls: ['./ad-project-info-modal.component.scss']
+})
+export class AdProjectInfoModalComponent {
+
+}
