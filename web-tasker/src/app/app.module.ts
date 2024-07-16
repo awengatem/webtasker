ab8ec27f604228993937758404f75da6583157ad
joe052 <joekaranjasenior52@gmail.com>
2024-07-16 03:29:35 +0300

    message

--- web-tasker/src/app/app.module.ts
+++ web-tasker/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { AngularMaterialModule } from './angular-material.module';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -9,24 +9,17 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { WebReqInterceptor } from './helpers/web-req.interceptor';
 import { NgInitDirective } from './load-directive';
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    NgInitDirective
-  ],
-  imports: [
-    BrowserModule,
-    HttpClientModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-
-    //material imports
-    AngularMaterialModule,
-  ],
-  providers: [
-    {provide: HTTP_INTERCEPTORS, useClass: WebReqInterceptor,multi: true}
-  ],
-  bootstrap: [AppComponent],
-  schemas: [CUSTOM_ELEMENTS_SCHEMA],
-})
+@NgModule({ declarations: [
+        AppComponent,
+        NgInitDirective
+    ],
+    bootstrap: [AppComponent],
+    schemas: [CUSTOM_ELEMENTS_SCHEMA], imports: [BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        //material imports
+        AngularMaterialModule], providers: [
+        { provide: HTTP_INTERCEPTORS, useClass: WebReqInterceptor, multi: true },
+        provideHttpClient(withInterceptorsFromDi())
+    ] })
 export class AppModule {}
