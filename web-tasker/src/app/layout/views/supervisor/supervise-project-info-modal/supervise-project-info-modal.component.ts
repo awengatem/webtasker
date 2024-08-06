192e89d63dc6c30f2c293e5bd2f1973bc6811593
joe052 <joekaranjasenior52@gmail.com>
2024-08-06 05:07:47 +0300

    message

---
+++ web-tasker/src/app/layout/views/supervisor/supervise-project-info-modal/supervise-project-info-modal.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-supervise-project-info-modal',
+  standalone: true,
+  imports: [],
+  templateUrl: './supervise-project-info-modal.component.html',
+  styleUrl: './supervise-project-info-modal.component.scss'
+})
+export class SuperviseProjectInfoModalComponent {
+
+}
