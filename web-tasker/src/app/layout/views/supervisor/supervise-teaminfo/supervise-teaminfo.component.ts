a32438f78d52178f1d9e2d9de8c7ff7be4316788
joe052 <joekaranjasenior52@gmail.com>
2024-08-08 05:07:18 +0300

    message

---
+++ web-tasker/src/app/layout/views/supervisor/supervise-teaminfo/supervise-teaminfo.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-supervise-teaminfo',
+  standalone: true,
+  imports: [],
+  templateUrl: './supervise-teaminfo.component.html',
+  styleUrl: './supervise-teaminfo.component.scss'
+})
+export class SuperviseTeaminfoComponent {
+
+}
