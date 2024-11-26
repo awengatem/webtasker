96a115f2893d679cf62b44af0f0e36c90ceadafc
awengatem <awengjohnatem@gmail.com>
2024-11-26 17:48:37 +0300

    changes

--- web-tasker/src/app/layout/views/supervisor/supervise-main-page2/supervise-main-page2.component.ts
+++ web-tasker/src/app/layout/views/supervisor/supervise-main-page2/supervise-main-page2.component.ts
@@ -5,8 +5,14 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './supervise-main-page2.component.html',
-  styleUrl: './supervise-main-page2.component.scss'
+  styleUrl: './supervise-main-page2.component.scss',
 })
 export class SuperviseMainPage2Component {
+  supTeamCount = 0;
+  supProjectCount = 0;
 
+  earningsAmount = 20000; // Replace this with your actual earnings data
+  theDifference = 30;
+  currentTime = '11:00:00:11';
+  todayDate = new Date().toString().split(' GMT')[0];
 }
